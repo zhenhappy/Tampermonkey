fdb419a9d38914381664d3e14c1bd65cef6359d2
zhenhappy <q505507538@gmail.com>
2018-10-04 23:19:53 +0800

    update

--- MSDN 我告诉你 清爽特别版.user.js
+++ MSDN 我告诉你 清爽特别版.user.js
@@ -1,30 +1,54 @@
 // ==UserScript==
 // @name         MSDN 我告诉你 清爽特别版
 // @namespace    https://greasyfork.org/zh-CN/scripts/370789
-// @version      0.2
+// @version      0.3
 // @description  MSDN 我告诉你 去除广告, 界面宽度100%(解决默写内容太长看不到的问题), 去除捐赠提示
 // @author       zhenhappy
-// @match        https://msdn.itellyou.cn/*
+// @match        http*://msdn.itellyou.cn/*
+// @require      https://code.jquery.com/jquery-3.3.1.slim.min.js
 // ==/UserScript==
 
+$(document).ready(() => {
+  document.cookie = 'never_show_donate_auto=true;expires=Fri, 31 Dec 9999 23:59:59 GMT;path=/'
+  $('#view_data_container img').remove()
+  $('.container').css('width', '100%')
+  $('.panel-title a').css('text-decoration', 'none')
+  $('.list-unstyled label, span.label, .panel').css('user-select', 'none')
+  $('.list-unstyled label, span.label, .panel').css('-webkit-user-select', 'none')
+  $('.list-unstyled label, span.label, .panel').css('-moz-user-select', 'none')
+  $('.list-unstyled label, span.label, .panel').css('-ms-user-select', 'none')
+  $(document).off('DOMSubtreeModified').on('DOMSubtreeModified', e => {
+    const current = $(e.target)
 
-(function() {
-    'use strict';
-    const $ = document.querySelector.bind(document);
-    const $$ = document.querySelectorAll.bind(document);
+    $('.list-unstyled label, span.label, .panel').css('user-select', 'none')
+    $('.list-unstyled label, span.label, .panel').css('-webkit-user-select', 'none')
+    $('.shang_box').remove()
+    $('.detail > pre:nth-child(3)').remove()
 
-    remove('#view_data_container img')
-    $$(".container").forEach(el => el.style.width = "100%")
-    document.cookie="never_show_donate_auto=true;expires=Fri, 31 Dec 9999 23:59:59 GMT;path=/"
-    document.addEventListener("DOMSubtreeModified", e => {
-        remove($$(".shang_box"));
-        remove($$('.detail > pre:nth-child(3)'));
-    }, false)
-
-    function remove(el) {
-    if (typeof el === "string") remove([...$$(el)]);
-    else if (el instanceof HTMLElement) el.parentNode.removeChild(el);
-    else if (el instanceof NodeList) remove([...el]);
-    else if (el instanceof Array) el.forEach(remove);
-}
-})();
+    if (current.attr('class') === 'detail' && current.children('textarea').length > 0) return
+    if (current.attr('class') === 'detail' && current.children('pre').length > 0) {
+      current.children('textarea').remove()
+      const textarea = document.createElement('textarea')
+      textarea.value = current.children('pre').text()
+      textarea.style.width = '100%'
+      textarea.style.height = '42px'
+      textarea.style.fontSize = '13px'
+      textarea.style.lineHeight = '35px'
+      textarea.style.color = '#333'
+      textarea.style.wordBreak = 'break-all'
+      textarea.style.wordWrap = 'break-word'
+      textarea.style.backgroundColor = '#f5f5f5'
+      textarea.style.border = '1px solid #ccc'
+      textarea.style.borderRadius = '4px'
+      textarea.style.outline = 'none'
+      textarea.style.resize = 'none'
+      textarea.onclick = () => {
+        textarea.select()
+        document.execCommand('copy')
+        console.log('复制成功')
+      }
+      current.append(textarea)
+      current.children('pre').remove()
+    }
+  })
+})
--- WEB前端开发博客 清爽特别版.user.js
+++ WEB前端开发博客 清爽特别版.user.js
@@ -1,36 +1,26 @@
 // ==UserScript==
 // @name         WEB前端开发博客 清爽特别版
 // @namespace    https://greasyfork.org/zh-CN/scripts/370792
-// @version      0.2
+// @version      0.3
 // @description  最可恶的就是这个了,  整个页面全他妈的都是广告,  正文内容就那么一小丁点,  你让我是来看广告还是学习的,  果断都干掉
 // @author       zhenhappy
-// @match        http://www.css88.com/*
+// @match        http*://www.css88.com/*
+// @require      https://code.jquery.com/jquery-3.3.1.slim.min.js
 // ==/UserScript==
 
-(function() {
-    'use strict';
-  
-    const $ = document.querySelector.bind(document);
-    const $$ = document.querySelectorAll.bind(document);
-  
-    const removeList = [
-        '#threebar',
-        '#secondary',
-        '.top-yideng-link',
-        '.basebreadcrumb',
-        '#home-page-widgets',
-        '.my-wallet',
-        '.related-wrap',
-        '.follow-wrap',
-        '.top-post-top-link',
-        "#site-navigation > ul > li:last-child"
-    ];
-    removeList.forEach(remove);
-  
-    function remove(el) {
-        if (typeof el === "string") remove([...$$(el)]);
-        else if (el instanceof HTMLElement) el.parentNode.removeChild(el);
-        else if (el instanceof NodeList) remove([...el]);
-        else if (el instanceof Array) el.forEach(remove);
-    }
-  })();
+$(document).ready(() => {
+  [
+    '#threebar',
+    '#secondary',
+    '.top-yideng-link',
+    '.basebreadcrumb',
+    '#home-page-widgets',
+    '.my-wallet',
+    '.related-wrap',
+    '.follow-wrap',
+    '.top-post-top-link',
+    '#site-navigation > ul > li:last-child'
+  ].forEach(item => {
+    $(item).remove()
+  })
+})
