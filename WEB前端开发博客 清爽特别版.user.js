f92e5dd3606f213add662126cc5581c61aa13df8
zhenhappy <q505507538@gmail.com>
2018-10-07 11:30:45 +0800

    update

--- WEB前端开发博客 清爽特别版.user.js
+++ WEB前端开发博客 清爽特别版.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WEB前端开发博客 清爽特别版
 // @namespace    https://greasyfork.org/zh-CN/scripts/370792
-// @version      0.3
+// @version      0.4
 // @description  最可恶的就是这个了,  整个页面全他妈的都是广告,  正文内容就那么一小丁点,  你让我是来看广告还是学习的,  果断都干掉
 // @author       zhenhappy
 // @match        http*://www.css88.com/*
@@ -19,8 +19,6 @@ $(document).ready(() => {
     '.related-wrap',
     '.follow-wrap',
     '.top-post-top-link',
-    '#site-navigation > ul > li:last-child'
-  ].forEach(item => {
-    $(item).remove()
-  })
+    "#site-navigation > ul > li:last-child"
+  ].forEach(item => $(item).remove())
 })
