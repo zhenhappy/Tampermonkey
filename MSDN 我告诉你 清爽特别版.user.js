767a18441aa7d8304fc1ce242b5d6c1ccbf69722
zhenhappy <q505507538@gmail.com>
2018-10-07 15:57:53 +0800

    update

--- MSDN 我告诉你 清爽特别版.user.js
+++ MSDN 我告诉你 清爽特别版.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MSDN 我告诉你 清爽特别版
 // @namespace    https://greasyfork.org/zh-CN/scripts/370789
-// @version      0.4
+// @version      0.5
 // @description  MSDN 我告诉你 去除广告, 界面宽度100%(解决默写内容太长看不到的问题), 去除捐赠提示
 // @author       zhenhappy
 // @match        http*://msdn.itellyou.cn/*
@@ -14,15 +14,21 @@ $(document).ready(() => {
   $('#view_data_container img').remove()
   $('.container').css('width', '100%')
   $('.panel-title a').css('text-decoration', 'none')
-  $('.list-unstyled label, span.label, .panel').css('user-select', 'none')
-  $('.list-unstyled label, span.label, .panel').css('-webkit-user-select', 'none')
-  $('.list-unstyled label, span.label, .panel').css('-moz-user-select', 'none')
-  $('.list-unstyled label, span.label, .panel').css('-ms-user-select', 'none')
+  $('.list-unstyled label, span.label, .panel').css({
+    '-webkit-user-select': 'none',
+    '-moz-user-select': 'none',
+    '-ms-user-select': 'none',
+    'user-select': 'none',
+  })
   $(document).off('DOMSubtreeModified').on('DOMSubtreeModified', e => {
     const current = $(e.target)
 
-    $('.list-unstyled label, span.label, .panel').css('user-select', 'none')
-    $('.list-unstyled label, span.label, .panel').css('-webkit-user-select', 'none')
+    $('.list-unstyled label, span.label, .panel').css({
+      '-webkit-user-select': 'none',
+      '-moz-user-select': 'none',
+      '-ms-user-select': 'none',
+      'user-select': 'none',
+    })
     $('.shang_box').remove()
     $('.detail > pre:nth-child(3)').remove()
 
@@ -32,17 +38,19 @@ $(document).ready(() => {
       const textarea = document.createElement('textarea')
       textarea.minRows = '1'
       textarea.value = current.children('pre').text()
-      textarea.style.width = '100%'
-      textarea.style.padding = '10px 10px'
-      textarea.style.fontSize = '14px'
-      textarea.style.color = '#333'
-      textarea.style.wordBreak = 'break-all'
-      textarea.style.wordWrap = 'break-word'
-      textarea.style.backgroundColor = '#f5f5f5'
-      textarea.style.border = '1px solid #ccc'
-      textarea.style.borderRadius = '4px'
-      textarea.style.outline = 'none'
-      textarea.style.resize = 'none'
+      $(textarea).css({
+        'width': '100%',
+        'padding': '10px',
+        'font-size': '14px',
+        'color': '#333',
+        'word-break': 'break-all',
+        'word-wrap': 'break-word',
+        'background-color': '#f5f5f5',
+        'border': '1px solid #ccc',
+        'border-radius': '4px',
+        'outline': 'none',
+        'resize': 'none'
+      })
       textarea.onclick = () => {
         textarea.select()
         document.execCommand("copy")
