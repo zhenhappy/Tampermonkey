104fade88db0bc8095c8ca37f83a646832239b05
zhenhappy <q505507538@gmail.com>
2018-10-08 14:42:50 +0800

    :hammer: update

--- WEB前端开发博客 清爽特别版.user.js
+++ WEB前端开发博客 清爽特别版.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WEB前端开发博客 清爽特别版
 // @namespace    https://greasyfork.org/zh-CN/scripts/370792
-// @version      0.5
+// @version      0.6
 // @description  最可恶的就是这个了,  整个页面全他妈的都是广告,  正文内容就那么一小丁点,  你让我是来看广告还是学习的,  果断都干掉
 // @author       zhenhappy
 // @run-at       document-start
