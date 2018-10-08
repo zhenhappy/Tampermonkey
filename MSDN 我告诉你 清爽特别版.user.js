8292831854e0d9b0399d37a4639ddf5fb21760e1
zhenhappy <q505507538@gmail.com>
2018-10-08 12:12:53 +0800

    :sparkles: update

--- MSDN 我告诉你 清爽特别版.user.js
+++ MSDN 我告诉你 清爽特别版.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         MSDN 我告诉你 清爽特别版
 // @namespace    https://greasyfork.org/zh-CN/scripts/370789
-// @version      0.9
-// @description  MSDN 我告诉你 去除广告, 界面宽度100%(解决SideBar里面的内容太长看不到的问题), 去除捐赠提示
+// @version      0.10
+// @description  MSDN 我告诉你 去除广告, 界面宽度100%(解决SideBar里面的内容太长看不到的问题), 去除捐赠提示, 点击链接自动复制
 // @author       zhenhappy
 // @run-at       document-start
 // @match        http*://msdn.itellyou.cn/*
