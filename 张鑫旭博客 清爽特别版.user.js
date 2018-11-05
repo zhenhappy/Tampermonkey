5aad0fe052ac4578e42ed35946526fe6dd6ec915
zhenhappy <q505507538@gmail.com>
2018-11-05 11:00:08 +0800

    update

--- 张鑫旭博客 清爽特别版.user.js
+++ 张鑫旭博客 清爽特别版.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         张鑫旭博客 清爽特别版
 // @namespace    https://greasyfork.org/zh-CN/scripts/370790
-// @version      0.4
+// @version      0.5
 // @description  张鑫旭这小子上面广告各种动态生成, 相比一般的广告技术来说还是贼牛逼的, 一般的ADBlocker去不掉, 所以通过脚本一个个去掉了, 并且将正文宽度调整到最佳合观看的角度, 让你心无旁骛的投入到愉快学习当中
 // @author       zhenhappy
 // @run-at       document-start
@@ -12,24 +12,28 @@
 document.documentElement.style.display = 'none'
 
 $(document).ready(() => {
-    'use strict';
+  'use strict'
 
-    $('#sidebar, .da_title, .da_col2, .link, .ins_txt, #daITxz2, .hd_remind, .description').remove()
+  $('#sidebar, .da_title, .da_col2, .link, .ins_txt, #daITxz2, .hd_remind, .description').remove()
 
-    $('#content').css({
-        'margin-left': '0',
-        'margin-right': '0',
-    })
-    $('#content, #respond, .commentlist').css({
-        'padding-left': '0',
-        'padding-right': '0'
-    })
-    $('#content > .post, #respond, .commentlist').css({
-        'margin-left': 'auto',
-        'margin-right': 'auto',
-        'width': '1440px',
-        'max-width': '90%'
-    })
+  $('#content').css({
+    'margin-left': '0',
+    'margin-right': '0',
+  })
+  $('#content, #respond, .commentlist').css({
+    'padding-left': '0',
+    'padding-right': '0'
+  })
+  $('#content > .post, #respond, .commentlist').css({
+    'margin-left': 'auto',
+    'margin-right': 'auto',
+    'width': '1440px',
+    'max-width': '90%'
+  })
+  $(window).bind('scroll', () => {
+    if ($(window).scrollTop() > 128) $('#entryNav').css('top', '20px')
+    else $('#entryNav').css('top', '150px')
+  })
 
-    document.documentElement.style.display = ''
+  document.documentElement.style.display = ''
 })
