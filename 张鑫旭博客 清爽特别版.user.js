8491533ea43956cc5a37af826de86ffa5913c790
zhenhappy <q505507538@gmail.com>
2018-11-05 15:20:16 +0800

    update

--- 张鑫旭博客 清爽特别版.user.js
+++ 张鑫旭博客 清爽特别版.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         张鑫旭博客 清爽特别版
 // @namespace    https://greasyfork.org/zh-CN/scripts/370790
-// @version      0.5
+// @version      0.6
 // @description  张鑫旭这小子上面广告各种动态生成, 相比一般的广告技术来说还是贼牛逼的, 一般的ADBlocker去不掉, 所以通过脚本一个个去掉了, 并且将正文宽度调整到最佳合观看的角度, 让你心无旁骛的投入到愉快学习当中
 // @author       zhenhappy
 // @run-at       document-start
@@ -30,9 +30,10 @@ $(document).ready(() => {
     'width': '1440px',
     'max-width': '90%'
   })
+  $('#entryNav').css('top', '150px')
   $(window).bind('scroll', () => {
-    if ($(window).scrollTop() > 128) $('#entryNav').css('top', '20px')
-    else $('#entryNav').css('top', '150px')
+    if ($(window).scrollTop() > 130) $('#entryNav').css('top', '20px')
+    else $('#entryNav').css('top', (150 - $(window).scrollTop()) + 'px')
   })
 
   document.documentElement.style.display = ''
