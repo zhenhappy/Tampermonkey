49059722c810c15de7f49c34dbe1dae2477dfd4b
zhenhappy <q505507538@gmail.com>
2021-02-04 16:09:17 +0800

    update

--- MDN 自动切换中文.user.js
+++ MDN 自动切换中文.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MDN 自动切换中文
 // @namespace    https://greasyfork.org/zh-CN/scripts/421190
-// @version      0.1
+// @version      0.2
 // @description  谷歌搜索的时候经常点击MDN文档打开的默认都是英文的, 很不方便, 来一个自动跳转, 如果需要其他语言, 手动点页面切换即可
 // @author       You
 // @match        https://developer.mozilla.org/**
