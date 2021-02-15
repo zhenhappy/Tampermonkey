c095cb567b0fa7762c8a2926b2c7f01fb4ebf79e
zhenhappy <q505507538@gmail.com>
2021-02-15 23:14:59 +0800

    i

--- 游戏年轮下载优化.user.js
+++ 游戏年轮下载优化.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         游戏年轮下载优化
 // @namespace    https://greasyfork.org/zh-CN/scripts/421664
-// @version      0.2
+// @version      0.3
 // @description  游戏年轮自动对下载地址做超链接, 方便点击, 验证码和密码均支持点击复制到剪贴板, 遇到未评论的会自动评论
 // @author       You
 // @match        http*://www.bibgame.com/*
@@ -12,41 +12,49 @@
 
 (function() {
     var t = setInterval(function () {
-        if ($('#chakan').html()) {
-            var url, code, password
-            $.each($('#chakan p'), function( index, value ) {
-                if (/http/g.test($(this).text())) {
-                    url = /(http.*?)\s/g.exec($(this).text())[1]
-                    $('#chakan p').eq(0).html('链接：<a style="border: none;text-decoration: underline;" href="' + url + '" target="_blank">' + url + '</a>')
-                }
-                if (/提取码/g.test($(this).text())) {
-                    code = /(\w+)/g.exec($(this).text())[1]
-                    $('#chakan p').eq(0).html('链接：<a style="border: none;text-decoration: underline;" href="' + url + '#' + code + '" target="_blank">' + url + '</a>')
-                    $(this).html('提取码：<span style="cursor: pointer;" id="code" data-clipboard-target="#code">'+code+'</span>')
-                    new ClipboardJS('#code')
-                }
-                if (/解压密码/g.test($(this).text())) {
-                    password = /解压密码：(.*?)$/g.exec($(this).text())[1]
-                    $(this).html('解压密码：<span style="cursor: pointer;" id="password" data-clipboard-target="#password">'+password+'</span>')
-                    new ClipboardJS('#password')
-                }
-                if (/发表评论后/g.test($(this).text())) {
-                    console.log('发表评论后')
-                    if ($('#pl-520am-f-saytext')) {
+        if ($('#chakan').html() && $('#pl-520am-f-saytext')) {
+            try {
+                var _url, url, _code, code, _password, password
+                $.each($('#chakan p'), function( index, value ) {
+                    _url = /(http.*?)\s/g.exec($(this).text())
+                    if (_url && _url.length > 1) {
+                        url = _url[1]
+                        console.log('url:', url)
+                        if (url) {
+                            $('#chakan p').eq(0).html('链接：<a style="border: none;text-decoration: underline;" href="' + url + '" target="_blank">' + url + '</a>')
+                        }
+                    }
+                    _code = /提取码：(.+)/g.exec($(this).text())
+                    if (_code && _code.length > 1) {
+                        code = _code[1]
+                        console.log('code:', code)
+                        if (code) {
+                            $('#chakan p').eq(0).html('链接：<a style="border: none" href="' + url + '#' + code + '" target="_blank">' + url + '</a>')
+                            $(this).html('提取码：<span style="cursor: pointer;" id="code" data-clipboard-target="#code">'+code+'</span>')
+                            new ClipboardJS('#code')
+                        }
+                    }
+                    _password = /解压密码：(.+)/g.exec($(this).text())
+                    if (_password && _password.length > 1) {
+                        password = _password[1]
+                        console.log('password:', password)
+                        if (password) {
+                            $(this).html('解压密码：<span style="cursor: pointer;" id="password" data-clipboard-target="#password">'+password+'</span>')
+                            new ClipboardJS('#password')
+                        }
+                    }
+                    if (/发表评论后/g.test($(this).text())) {
+                        console.log('发表评论后')
                         $('#pl-520am-f-saytext').val('感谢分享')
                         $('#pl-submit-btn-main').click()
                         window.location.reload()
                     }
-                }
-            })
-            console.log('url:', url)
-            console.log('code:', code)
-            console.log('password:', password)
-
-            clearInterval(t)
+                })
+            } catch (e) {
+                console.error(e)
+            } finally {
+                clearInterval(t)
+            }
         }
     }, 500)
-
-    //$('#chakan p').eq(0).html($('#chakan p').eq(0).text().replace(/(http.*?)\s/g, '<a href="$1">$1</a>'))
-    // Your code here...
-})();
+})()
