5b7b6e56fa94b17ea88849b9ab24b41b6264b23e
zhenhappy <q505507538@gmail.com>
2021-04-07 10:09:52 +0800

    i

--- macpeers下载优化.user.js
+++ macpeers下载优化.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         macpeers优化
 // @namespace    https://greasyfork.org/zh-CN/scripts/424268
-// @version      0.4
+// @version      0.5
 // @description  macpeers自动对下载地址做超链接, 方便点击, 验证码和密码均支持点击复制到剪贴板
 // @author       zhenhappy<q505507538@gmail.com>
 // @icon         https://hkmacpeers-1259420093.file.myqcloud.com/favicon.ico
@@ -14,19 +14,22 @@
     console.log('macpeers>>>>>>>>')
     var t = setInterval(function () {
         try {
-            if ($('.post_content .STYLE2').length > 0) {
+            var content = $('.post_content .STYLE2, .post_content .STYLE5')
+            if (content.length > 0) {
+                console.log('找到下载地址, 正在处理下载链接...')
                 var _url, url, _code, code, _password, password
-                $.each($('.post_content .STYLE2'), function( index, value ) {
+                $.each(content, function( index, value ) {
                     _url = /(http.*?)\s/g.exec($(this).text())
-                    _code = /提取码\: (\w+)/g.exec($(this).text())
-                    _password = /解压密码：(\w+)/g.exec($(this).text())
+                    console.log(_url)
+                    _code = /提取码.*?(\w+)\s*/g.exec($(this).text())
+                    _password = /解压密码.*?(\w+)\s*/g.exec($(this).text())
                     if (_url && _url.length > 1 && _code && _code.length > 1) {
                         url = _url[1]
                         console.log('url:', url)
                         code = _code[1]
                         console.log('code:', code)
                         if (code) {
-                            $(this).html('链接: <a href="' + url + '#' + code + '" target="_blank" rel="nofollow noopener noreferrer">' + url + '</a> 提取码: <span style="cursor: pointer;" id="code" data-clipboard-target="#code">'+code+'</span>')
+                            $(this).html('链接: <a href="' + url + '#' + code + '" target="' + url + '#' + code + '" rel="nofollow noopener noreferrer">' + url + '</a> 提取码: <span style="cursor: pointer;" id="code" data-clipboard-target="#code">'+code+'</span>')
                             new ClipboardJS('#code')
                         }
                     }
