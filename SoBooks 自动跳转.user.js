951dadcb4b5345a61568f90f178917617f2b22ab
ZhenHappy <q505507538@gmail.com>
2021-10-22 15:01:44 +0800

    更新

--- SoBooks 自动跳转.user.js
+++ SoBooks 自动跳转.user.js
@@ -1,16 +1,13 @@
 // ==UserScript==
 // @name         SoBooks 自动跳转
 // @namespace    https://greasyfork.org/zh-CN/scripts/434291
-// @version      0.1
+// @version      0.2
 // @description  SoBooks 网址中转免等待自动跳转
-// @author       You
+// @author       zhenhappy<q505507538@gmail.com>
 // @match        https://sobooks.cc/go.html?url=*
 // @icon         https://sobooks.cc/favicon.ico
-// @grant        none
 // ==/UserScript==
 
 (function() {
-    'use strict';
     window.location.replace(window.location.href.replace('https://sobooks.cc/go.html?url=',''))
-    // Your code here...
 })();
