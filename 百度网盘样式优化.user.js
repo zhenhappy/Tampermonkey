b7cefeba2a86b671f68206ecbac62d1d4bd285be
zhenhappy <q505507538@gmail.com>
2021-11-12 22:17:30 +0800

    更新

--- 百度网盘样式优化.user.js
+++ 百度网盘样式优化.user.js
@@ -1,16 +1,18 @@
 // ==UserScript==
 // @name         百度网盘样式优化
 // @namespace    https://greasyfork.org/zh-CN/scripts/433769
-// @version      0.2
+// @version      0.3
 // @description  解决百度网盘页面文件名称显示不完整
-// @author       You
+// @author       zhenhappy<q505507538@gmail.com>
 // @match        https://pan.baidu.com/s/*
 // @icon         https://pan.baidu.com/m-static/base/static/images/favicon.ico
 // @grant        none
 // ==/UserScript==
- 
+
 (function() {
-    document.querySelector('#layoutMain').style.minWidth = '1920px'
+    $('.slide-show-left').css('float', 'unset')
+    $('.slide-show-left .file-name').css('max-width', 'unset')
+    // document.querySelector('#layoutMain').style.minWidth = '1280px'
     document.querySelector('#layoutMain > div.frame-content > div.module-share-header > div > div.slide-show-left > h2').addEventListener('DOMSubtreeModified', function () {
         document.querySelector('#layoutMain > div.frame-content > div.module-share-header > div > div.slide-show-left > h2').removeChild(document.querySelector('#layoutMain > div.frame-content > div.module-share-header > div > div.slide-show-left > h2 > img'))
         var span = document.createElement('span')
