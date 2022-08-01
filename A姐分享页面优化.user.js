5f7729ae58b1fe6c8f72f36d5b5117dcf361d6e5
zhenhappy <q505507538@gmail.com>
2022-08-01 09:49:26 +0800

    A姐分享页面优化

--- A姐分享页面优化.user.js
+++ A姐分享页面优化.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         A姐分享 页面优化
 // @namespace    https://greasyfork.org/en/scripts/443351
-// @version      0.3
+// @version      0.4
 // @description  解除复制限制
 // @author       zhenhappy<q505507538@gmail.com>
-// @match        https://www.abskoop.com/*/
+// @match        https://*.abskoop.com/*/
+// @match        https://*.ahhhhfs.com/*/
 // @icon         https://www.abskoop.com/wp-content/uploads/2021/07/1625221481-04bb5153c0db541-192x192.webp
 // @require      https://unpkg.com/jquery/dist/jquery.slim.min.js
 // @grant        none
