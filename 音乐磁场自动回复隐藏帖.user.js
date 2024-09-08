ee443c45870d3c5da14f5e49812b4bd6392efbeb
zhenhappy <q505507538@gmail.com>
2024-09-09 05:08:21 +0800

    Changes

--- 音乐磁场自动回复隐藏帖.user.js
+++ 音乐磁场自动回复隐藏帖.user.js
@@ -2,12 +2,13 @@
 // @name         音乐磁场自动回复隐藏帖
 // @namespace    http://tampermonkey.net/
 // @version      2024-09-07
-// @description  音乐磁场自动回复隐藏帖
+// @description  自动回复音乐磁场论坛上的隐藏帖
 // @author       zhenhappy<q505507538@gmail.com>
 // @match        https://www.hifini.com/thread-*.htm
 // @icon         https://www.hifini.com/favicon.ico
 // @require      https://unpkg.com/jquery/dist/jquery.slim.min.js
 // @run-at       document-body
+// @license      MIT
 // ==/UserScript==
 
 (function() {
