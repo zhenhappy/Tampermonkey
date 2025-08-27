886d53e9463effb0e6bfd5c7494c6c733a3fca14
zhenhappy <q505507538@gmail.com>
2025-08-27 10:42:38 +0800

    更新版本号至1.2.1，新增对 https://*.hifiki.com 的支持。

--- 音乐磁场自动回复隐藏帖.user.js
+++ 音乐磁场自动回复隐藏帖.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         音乐磁场自动回复隐藏帖
 // @namespace    https://greasyfork.org/zh-CN/scripts/507531
-// @version      1.2
+// @version      1.2.1
 // @description  自动回复音乐磁场论坛上的隐藏帖
 // @author       zhenhappy<q505507538@gmail.com>
 // @match        https://*.hifiti.com/thread-*.htm
+// @match        https://*.hifiki.com/thread-*.htm
 // @icon         https://www.hifini.com/favicon.ico
 // @require      https://unpkg.com/jquery/dist/jquery.slim.min.js
 // @run-at       document-body
